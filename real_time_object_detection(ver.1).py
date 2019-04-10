058f53eb37c9c118b4e91b4c5ca2c96b2e8195d3
Faragon02 <45257459+Faragon02@users.noreply.github.com>
2019-04-10 23:04:44 +0900

    Update real_time_object_detection(ver.1).py

--- real_time_object_detection(ver.1).py
+++ real_time_object_detection(ver.1).py
@@ -1,26 +1,23 @@
-# USAGE
-# python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
-# import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
 import numpy as np
 import argparse
 import imutils
-import time
 import cv2
+import io
+import socket
+import struct
+import time
+import pickle
+import zlib
 
-
-# construct the argument parse and parse the arguments
-def im():
-
+def independence_object():
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
 	ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
 	ap.add_argument("-c", "--confidence", type=float, default=0.2, help="minimum probability to filter weak detections")
 	args = vars(ap.parse_args())
 
-	# initialize the list of class labels MobileNet SSD was trained to
-	# detect, then generate a set of bounding box colors for each class
 	CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 			   "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 			   "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
@@ -28,84 +25,128 @@ def im():
 
 	COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-	# load our serialized model from disk
 	print("[INFO] loading model...")
 	net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-	# initialize the video stream, allow the cammera sensor to warmup,
-	# and initialize the FPS counter
 	print("[INFO] starting video stream...")
 	vs = VideoStream(src=0).start()
-	# vs = VideoStream(usePiCamera=True).start()
 	time.sleep(2.0)
 	fps = FPS().start()
-	f = 0
-	# loop over the frames from the video stream
-	while True:
-		# grab the frame from the threaded video stream and resize it
-		# to have a maximum width of 400 pixels
-		frame = vs.read()
-		frame = imutils.resize(frame, width=400)
 
-		# grab the frame dimensions and convert it to a blob
+
+	while True:
+		frame = vs.read()
+		frame = imutils.resize(frame, width=300)
+
 		(h, w) = frame.shape[:2]
 		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
 
-		# pass the blob through the network and obtain the detections and
-		# predictions
 		net.setInput(blob)
 		detections = net.forward()
 
-		# loop over the detections
 		for i in np.arange(0, detections.shape[2]):
-			# extract the confidence (i.e., probability) associated with
-			# the prediction
 			confidence = detections[0, 0, i, 2]
 
-			# filter out weak detections by ensuring the `confidence` is
-			# greater than the minimum confidence
 			if confidence > args["confidence"]:
-				# extract the index of the class label from the
-				# `detections`, then compute the (x, y)-coordinates of
-				# the bounding box for the object
 				idx = int(detections[0, 0, i, 1])
 				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 				(startX, startY, endX, endY) = box.astype("int")
 
-				# draw the prediction on the frame
 				persent = confidence * 100
 				label = "{}: {:.2f}%".format(CLASSES[idx], persent)
-				if idx == 15 and persent >= 90:
-					bmage(frame)
-				f = + 1
+
 				cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[idx], 2)
 				y = startY - 15 if startY - 15 > 15 else startY + 15
 				cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-		# show the output frame
+		cv2.imshow("Frame", frame)
 		key = cv2.waitKey(1) & 0xFF
-
-		# if the `q` key was pressed, break from the loop
 		if key == ord("q"):
 			break
-		# update the FPS counter
 		fps.update()
 
-	# stop the timer and display FPS information
 	fps.stop()
 	print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 	print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-	# do a bit of cleanup
 	cv2.destroyAllWindows()
 	vs.stop()
 
-def bmage(frame):
+def im(host, port):
+	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	client_socket.connect((host,port))
+	connection = client_socket.makefile('wb')
+
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
+	ap.add_argument("-m", "--model", required=True, help="path to Caffe pre-trained model")
+	ap.add_argument("-c", "--confidence", type=float, default=0.2, help="minimum probability to filter weak detections")
+	args = vars(ap.parse_args())
+
+	CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+			   "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+			   "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+			   "sofa", "train", "tvmonitor"]
+
+	COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+	print("[INFO] loading model...")
+	net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+	print("[INFO] starting video stream...")
+	vs = VideoStream(src=0).start()
+	time.sleep(2.0)
+	fps = FPS().start()
+
+	img_counter = 0
+	encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 	while True:
+		frame = vs.read()
+		frame = imutils.resize(frame, width=300)
+
+		(h, w) = frame.shape[:2]
+		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
+
+		net.setInput(blob)
+		detections = net.forward()
+		for i in np.arange(0, detections.shape[2]):
+
+			confidence = detections[0, 0, i, 2]
+
+			if confidence > args["confidence"]:
+				idx = int(detections[0, 0, i, 1])
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
+
+				persent = confidence * 100
+				label = "{}: {:.2f}%".format(CLASSES[idx], persent)
+				if idx ==15 and persent >=90:
+					result, frame = cv2.imencode('.jpg', frame, encode_param)
+
+					data = pickle.dumps(frame, 0)
+					size = len(data)
+					print("{}: {}".format(img_counter, size))
+					time.sleep(0.1)
+					client_socket.sendall(struct.pack(">L", size) + data)
+					img_counter += 1
+
+				cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[idx], 2)
+				y = startY - 15 if startY - 15 > 15 else startY + 15
+				cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 		cv2.imshow("Frame", frame)
-		encode_param = [int(cv2.IMWRITE_JPEG_CHROMA_QUALITY), 90]
-		result, imgencode = cv2.imencode('.jpg', frame, encode_param)
-		data = np.array(imgencode)
-		strdata = data.tostring()
-		print(str(strdata))
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("q"):
+			break
+		fps.update()
+
+	fps.stop()
+	print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+	print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+	cv2.destroyAllWindows()
+	vs.stop()
 
 if __name__ == '__main__':
-	a= im()
+	host = socket.gethostname()
+	port = 9999
+	if bool(im(host, port)) ==True:
+		im(host,port)
+	else:
+		time.sleep(0.5)
+		independence_object()
